2755d24b4ef687fa133087eb6f3fb00ae1815dda
mano8888 <manukambakam.k@gmail.com>
2019-06-24 23:57:24 -0700

    added all files

--- cont.sh
+++ cont.sh
@@ -1,13 +1,13 @@
 manoj=$1
 echo "creating a $manoj containers"
-sleep 2:
+sleep 2;
 
 for i in `seq $manoj`
 do
 echo "==================="
 echo "just now creating www.dog$i containers"
 sleep 1
-sudo docker run -it --name www.dog$i mano8888/mydoc /bin/bash
+sudo docker run -it --name www.dog$i -d mano8888/mydoc /bin/bash
 echo "==============="
 echo "www.dog$i containers are created"
 done
